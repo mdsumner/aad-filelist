918d237e56bf55978c333e12ac3d5f9ca00581a6
Michael Sumner <mdsumner@gmail.com>
2026-06-25 00:04:28 +0000

    remove temp stuff

--- inst/scheme/step-3-virtualization.R
+++ inst/scheme/step-3-virtualization.R
@@ -1,12 +1,6 @@
 Sys.setenv(HDF5_USE_FILE_LOCKING = "FALSE")
 
 library(blocklist)
-#vrt_remote <- "/vsicurl/https://projects.pawsey.org.au/aad-derived/oisst_mdim.vrt"
-#gdalraster::gdal_run("vsi copy", c("--source", vrt_remote, "--destination", tf <- tempfile(fileext = ".vrt")))
-#text <- readr::read_file(tf); text <- gsub("/vsicurl/https://", "/rdsi/PUBLIC/raad/data", text)
-#readr::write_file(text, vrt_local <- tempfile(fileext = ".vrt"))
-## set:
-#source("inst/scheme/step-1-define-set.R")
 
 src <- tibble::tibble(
   access = paste0("/rdsi/PUBLIC/raad/data/", set$file),  # local: read bytes / chunk index here
